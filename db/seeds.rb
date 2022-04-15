f4af14240511d3f5a52b7b2cfe0cfdb7020b6f11
arya mokhber <amokhber94@gmail.com>
2022-04-15 08:54:05 -0400

    Rails simple airbnb first push

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Flat.create(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: Faker::Number.number(digits: 2),
+    number_of_guests: Faker::Number.number(digits: 1)
+
+  )
+end
